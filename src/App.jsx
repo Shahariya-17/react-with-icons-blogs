83c81e3314d942b51f5622b02dd08bfd021f040a
Shahariya-17 <shahariyarrakib780@gmail.com>
2025-04-13 21:39:06 +0600

    added pricing cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 
+import { Suspense } from 'react'
 import './App.css'
 import DaisyNav from './components/DaisyNav/DaisyNav'
 import Navbar from './components/Navbar/Navbar'
+// import PricingOptions from './components/PricingOptions/PricingOptions'
+import PricingCard from './components/PricingCard/PricingCard'
+
+
+const pricingPromise = fetch('pricingData.json').then(res => res.json())
+
 
 function App() {
   
+ 
 
   return (
     <>
@@ -12,6 +20,20 @@ function App() {
         {/* <DaisyNav></DaisyNav> */}
         <Navbar></Navbar>
       </header>
+      <main className='grid md:grid-cols-2 lg:grid-cols-3 gap-10 mt-10'>
+      {/* <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
+          // <PricingOptions pricingPromise={pricingPromise}></PricingOptions>
+        </Suspense> */}
+        <PricingCard></PricingCard>
+        <PricingCard></PricingCard>
+        <PricingCard></PricingCard>
+        <PricingCard></PricingCard>
+        <PricingCard></PricingCard>
+        <PricingCard></PricingCard>
+        <PricingCard></PricingCard>
+        <PricingCard></PricingCard>
+        <PricingCard></PricingCard>
+      </main>
       
      
     </>
---
+++ src/components/PricingOptions/PricingOptions.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PricingOptions = ({pricingPromise}) => {
+
+    // const pricingData = use(pricingPromise);
+    // console.log(pricingData);
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PricingOptions;
